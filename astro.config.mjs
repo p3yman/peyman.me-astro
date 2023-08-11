1e7d3c5af4b4b06b818e57e86116371b8f65856f
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-08-11 14:34:42 +0200

    Add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://peyman.me",
+  integrations: [tailwind(), robotsTxt()],
   experimental: {
     assets: true,
   },
