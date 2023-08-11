09ac8dd726fdbb5ba27958adc1af0160d4267c32
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-08-11 11:38:08 +0200

    Add head meta

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blogCollection = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
+      description: z.string(),
       date: z.date(),
       cover: image().refine((img) => img.width >= 720, {
         message: "Cover image must be at least 720 pixels wide!",
