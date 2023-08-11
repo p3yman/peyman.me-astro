8ab2e6720082ca9a019557ccd47dc020c837be2c
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-08-11 14:37:39 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://peyman.me",
-  integrations: [tailwind(), robotsTxt()],
+  site: process.env.VERCEL_URL,
+  integrations: [tailwind(), robotsTxt(), sitemap()],
   experimental: {
     assets: true,
   },
