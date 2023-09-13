d519807b151cb4adda36e0a60f9e654a57801ff3
Peyman Eskandari <peyman.eskandari@gmail.com>
2023-09-13 15:45:29 +0200

    Set site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `https://${import.meta.env.VERCEL_URL}`,
+  site: `https://${import.meta.env.PUBLIC_VERCEL_URL}`,
   integrations: [tailwind(), robotsTxt(), sitemap()],
 });
